3675f4a30cf8598849d0f60a3a19cb2408ab3257
Jeremy Kerr <jk@codeconstruct.com.au>
2022-09-27 14:55:28 +0800

    powerpc/microwatt: Add SOC reset via syscon
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

--- arch/powerpc/platforms/microwatt/setup.c
+++ arch/powerpc/platforms/microwatt/setup.c
@@ -9,6 +9,7 @@
 #include <linux/stddef.h>
 #include <linux/init.h>
 #include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/of_platform.h>
 
 #include <asm/machdep.h>
@@ -18,9 +19,26 @@
 
 #include "microwatt.h"
 
+#define SYSCON_CTRL 0x28
+#define SYSCON_CTRL_DRAM_AT_0	BIT(0)
+#define SYSCON_CTRL_CORE_RESET	BIT(1)
+#define SYSCON_CTRL_SOC_RESET	BIT(2)
+
+static __iomem void *syscon_base;
+
 static void __init microwatt_init_IRQ(void)
 {
 	xics_init();
+}
+
+static void __noreturn microwatt_restart(char *cmd)
+{
+	if (syscon_base) {
+		u32 v = readl(syscon_base + SYSCON_CTRL);
+		writel(v | SYSCON_CTRL_SOC_RESET, syscon_base + SYSCON_CTRL);
+	}
+	for (;;)
+		;
 }
 
 static int __init microwatt_probe(void)
@@ -30,6 +48,14 @@ static int __init microwatt_probe(void)
 
 static int __init microwatt_populate(void)
 {
+	struct device_node *np;
+
+	np = of_find_compatible_node(NULL, NULL, "microwatt,syscon");
+	if (np) {
+		syscon_base = of_iomap(np, 0);
+		of_node_put(np);
+	}
+
 	return of_platform_default_populate(NULL, NULL, NULL);
 }
 machine_arch_initcall(microwatt, microwatt_populate);
@@ -46,4 +72,5 @@ define_machine(microwatt) {
 	.setup_arch		= microwatt_setup_arch,
 	.progress		= udbg_progress,
 	.calibrate_decr		= generic_calibrate_decr,
+	.restart		= microwatt_restart,
 };
